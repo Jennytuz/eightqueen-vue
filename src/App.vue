774373b37bfe668832d82ff6aa398a6fb6a91017
Jennytuz <490052745@qq.com>
2021-12-26 21:45:48 +0800

    init

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div>八皇后</div>
+    <eight-queen />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import EightQueen from './components/EightQueen.vue'
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    EightQueen
   }
 }
 </script>
---
+++ src/components/EightQueen.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <div class="title">
+      八皇后问题
+    </div>
+    <div class="grid">
+      <div v-for="(row, r_index) in grids" :key="r_index">
+        <div class="cell" v-for="(cell, c_index) in row" :key="c_index">
+          <div v-if="cell.ok">Q</div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+
+const grids = new Array(8).fill(1).map((_,r) => {
+  return new Array(8).fill(1).map((_, c) => {
+    return {
+      key: `key-${r*8+c}`,
+      ok: false
+    }
+  }) 
+})
+export default {
+  data () {
+    return {
+      grids
+    }
+  },
+}
+</script>
